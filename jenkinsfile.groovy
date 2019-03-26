ca6984fa192991e47b3f0a48baeba66fe4b4c12b
pkumargit <pankaj.kumar@marklogic.com>
2019-03-26 10:00:54 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,5 +1,13 @@
 node {
-  stage('Checkout SCM') {
-    checkout scm
+  stage('Checkout SCM in PUll Req') {
+    if (env.CHANGE_TARGET == "develop") {
+      checkout scm
+    }
+  }
+  
+  stage('Checkout SCM in code merge') {
+    if (env.BRANCH_NAME == "master") {
+      checkout scm
+    }
   }
 }
